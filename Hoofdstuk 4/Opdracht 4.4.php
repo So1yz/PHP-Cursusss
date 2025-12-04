b146bf473e87599ffe977ba28ac29550e97230a1
Yusuf <yusuf.simsek9080@gmail.com>
2025-12-04 13:16:32 +0100

    Update Opdracht 4.4.php

--- Hoofdstuk 4/Opdracht 4.4.php
+++ Hoofdstuk 4/Opdracht 4.4.php
@@ -2,16 +2,23 @@
 // auteur: Yusuf Simsek
 // functie: prijzen verhogen
 
-$prijs1 = 150;
-$oudePrijs = $prijs1;
+$prijs = 100;
 
-if ($prijs1 > 150) {
-    $prijs1 *= 1.19; // Verhoog met 19%
-} elseif ($prijs1 < 55) {
-    $prijs1 *= 1.11; // Verhoog met 11%
-} else {
-    $prijs1 *= 1.16; // Verhoog met 16%
+if ($prijs > 150) 
+{
+    $percentage = "19%";
+    $nieuwe_prijs = $prijs * 1.19;
+} 
+elseif ($prijs < 55) 
+{
+    $percentage = "11%";
+    $nieuwe_prijs = $prijs * 1.11;
+} 
+else 
+{
+    $percentage = "16%";
+    $nieuwe_prijs = $prijs * 1.16;
 }
-
-print "Oude prijs: €$oudePrijs<br>Nieuwe prijs: €" . round($prijs1, 2);
+echo "Oude prijs: € $prijs <br>";
+echo "Na verhoging van $percentage is de prijs: € $nieuwe_prijs";
 ?>
