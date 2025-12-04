4d101d5487b462209843b75e320aaf61347c5856
Yusuf <yusuf.simsek9080@gmail.com>
2025-12-04 13:00:44 +0100

    Create datatypes.php

---
+++ Opdracht 3/datatypes.php
@@ -0,0 +1,14 @@
+<?php
+
+$voornaam = "Yusuf";
+$achternaam = "Simsek"; 
+
+$x = 4;
+$y = 7;
+$z = 11;
+
+
+print " Voornaam: $voornaam <br>
+        Achternaam: $achternaam  <br>" ;
+
+print " $x + $y = $z";
